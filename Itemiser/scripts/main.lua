bd019b3fc8f9bfdb4042bbba3c4531c537a7a6e9
Duckos-Mods <businwes@hotmail.com>
2023-09-30 20:18:14 +0100

    Uploaded First Two Mods

---
+++ Itemiser/scripts/main.lua
@@ -0,0 +1,100 @@
+
+print("[itemiser] Loaded!")
+
+---@type boolean
+local foundWeaponMods = false;
+---@type boolean
+local foundPerks = false;
+---@type boolean
+local foundGrenadeMods = false;
+
+---@return table<UCrabWeaponModDA>
+function GetAllCWMDA()
+    ---@type table<UCrabWeaponModDA>
+    local CWMDA = FindAllOf("CrabWeaponModDA");
+    print("[itemiser] found " .. #CWMDA .. " weapon mods");
+    return CWMDA;
+end
+
+---@return table<UCrabPerkDA>
+function GetAllCPDA()
+    ---@type table<UCrabPerkDA>
+    local CPDA = FindAllOf("CrabPerkDA");
+    print("[itemiser] found " .. #CPDA .. " perks");
+    return CPDA;
+end
+
+---@return table<UCrabGrenadeModDA>
+function GetAllCGMDA()
+    ---@type table<UCrabGrenadeModDA>
+    local CGMDA = FindAllOf("CrabGrenadeModDA");
+    print("[itemiser] found " .. #CGMDA .. " grenade mods");
+    return CGMDA;
+end
+
+---@type table<UCrabWeaponModDA> 
+CWMDA = {};
+---@type table<UCrabPerkDA>
+CPDA = {};
+---@type table<UCrabGrenadeModDA>
+CGMDA = {};
+
+function GetAllModData()
+    if not foundWeaponMods then
+        CWMDA = GetAllCWMDA();
+        foundWeaponMods = true;
+    end
+    if not foundPerks then
+        CPDA = GetAllCPDA();
+        foundPerks = true;
+    end
+    if not foundGrenadeMods then
+        CGMDA = GetAllCGMDA();
+        foundGrenadeMods = true;
+    end
+end
+
+---@param Player ACrabPS
+function RandomiseWMD(Player)
+    print("[itemiser] randomising weapon mods for a player");
+    ---@param PFmod FCrabWeaponMod
+    Player.WeaponMods:ForEach(function(index, PFmod)
+        ---@type FCrabWeaponMod
+        local FMod = PFmod:get();
+        FMod.WeaponModDA = CWMDA[math.random(1, #CWMDA)];
+    end)
+
+    print("[itemiser] randomising perks for a player");
+    ---@param PFperk FCrabPerk
+    Player.Perks:ForEach(function(index, PFperk)
+        ---@type FCrabPerk
+        local FPerk = PFperk:get();
+        FPerk.PerkDA = CPDA[math.random(1, #CPDA)];
+    end)
+
+    print("[itemiser] randomising grenade mods for a player");
+    ---@param PFmod FCrabGrenadeMod
+    Player.GrenadeMods:ForEach(function(index, PFmod)
+        ---@type FCrabGrenadeMod
+        local FMod = PFmod:get();
+        FMod.GrenadeModDA = CGMDA[math.random(1, #CGMDA)];
+    end)
+end
+
+---@param CRAB ACrabPC
+---@param nim any
+function ModifyPlayerData(CRAB, nim)
+    print("[itemiser] player went through portal!")
+
+    GetAllModData();
+    ---@type table<ACrabPS>
+    local CrabPS = FindAllOf("CrabPS");
+    ---@param c ACrabPS
+    for i, c in ipairs(CrabPS) do
+        print(string.format("[itemiser] randomising player %s", i));
+        RandomiseWMD(c);
+    end
+end
+
+
+RegisterHook("/Script/CrabChampions.CrabPC:ClientOnEnteredPortal", ModifyPlayerData, CRAB, nim);
---
+++ Leveler/scripts/main.lua
@@ -0,0 +1,56 @@
+
+---@param CRAB ACrabPC
+---@param nim any
+function ModifyItemLevel(CRAB, nim)
+    print("Starting LoopChests");
+
+    ExecuteWithDelay(3000, function()
+        local CrabPS = FindAllOf("CrabPS");
+        if not CrabPS then
+            print("No CrabPS Found");
+            return;
+        else
+            print("Found CrabPS with " .. #CrabPS .. " entries");
+            print(string.format("user one has %s perk slots", CrabPS[1].NumPerkSlots));
+            ---@param v ACrabPS
+            for i, v in ipairs(CrabPS)  do
+                print(string.format("player %s has %s weapon mods", i, v.WeaponMods:GetArrayNum()));
+
+
+                ---@param wmod FCrabWeaponMod
+                v.WeaponMods:ForEach(function(index, wmod)
+                    ---@type FCrabWeaponMod
+                    local mod = wmod:get();
+                    mod.Level = math.random(0, 255);
+                    mod.WeaponModDA.WeaponModType = math.random(1, 83);
+                    -- wmod:set(mod);
+                end)
+                ---@param wmod FCrabGrenadeMod
+                v.GrenadeMods:ForEach(function(index, wmod)
+                    ---@type FCrabGrenadeMod
+                    local mod = wmod:get();
+                    mod.Level = math.random(0, 255);
+                    -- wmod:set(mod);
+                end)
+                ---@param wmod FCrabPerk
+                v.Perks:ForEach(function(index, wmod)
+                    ---@type FCrabPerk
+                    local mod = wmod:get();
+                    mod.Level = math.random(0, 255);
+                    -- wmod:set(mod);
+                end)
+            end
+        end
+    
+    end)
+end
+RegisterHook("/Script/CrabChampions.CrabPC:ClientOnEnteredPortal", ModifyItemLevel, CRAB, nim);
+
+
+
+
+
+
+
+
+-- NotifyOnNewObject("/Script/Engine.PlayerController", LoopChests, CreatedObject)
